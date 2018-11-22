10c753df130304bbb8c61c6b248f5cf18c053096
Philip Borenstein <pborenstein@gmail.com>
2018-11-22 04:25:04 -0500

    Netlify doesn't like async-git's .name

--- src/_data/git.js
+++ src/_data/git.js
@@ -5,7 +5,6 @@ module.exports = async function() {
  let ret =  {
    commitsha:   await git.short,
    curbranch:   await git.branch,
-   reponame:    await git.name,
    commitdate:  await git.date
  }
 
