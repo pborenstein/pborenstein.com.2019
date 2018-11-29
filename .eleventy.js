a160dc645dd495724afb6df712f41a5411e572c3
Philip Borenstein <pborenstein@gmail.com>
2018-11-29 00:36:05 -0500

    simplify head filter

--- .eleventy.js
+++ .eleventy.js
@@ -17,13 +17,8 @@ module.exports = function(eleventyConfig) {
 
 
   // Get the first `n` elements of a collection.
-  eleventyConfig.addFilter("head", (array, n) => {
-    if( n < 0 ) {
-      return array.slice(n);
-    }
-
-    return array.slice(0, n);
-  });
+  eleventyConfig.addFilter("head", (array, n) =>
+    n < 0 ? array.slice(n) : array.slice(0,n))
 
 
   //  The `articles` collection contains
