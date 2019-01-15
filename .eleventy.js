ea66fd7b0a42906cf357ea0a7d0624a05bc92305
Philip Borenstein <pborenstein@gmail.com>
2019-01-15 12:26:38 -0500

    No need to copy assets

--- .eleventy.js
+++ .eleventy.js
@@ -63,7 +63,6 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("src/img");
   eleventyConfig.addPassthroughCopy("src/css");
-  eleventyConfig.addPassthroughCopy("src/assets")
 
 /*  ===
   MARKDOWN PLUGINS
