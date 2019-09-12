e507887e487c52b83d96f9422cb67ebd1893a202
Philip Borenstein <pborenstein@gmail.com>
2019-09-12 04:07:09 -0400

    no debug for you

--- _11ty/pdump.js
+++ _11ty/pdump.js
@@ -1,6 +1,4 @@
 const decycle = require('json-cycle').decycle
-const debug = require("debug")("catlist");
-
 
 module.exports = function pdump(obj) {
   let decycled = JSON.stringify(decycle(obj), null, 2)
